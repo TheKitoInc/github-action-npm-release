80de831c0f169c63c2e608d5aa9a40e8bba1b81d
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2025-06-23 12:47:54 -0300

    format

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const core = require('@actions/core');
-const {Octokit} = require('@octokit/rest');
-const {join: pathJoin} = require('path');
-const {promisify} = require('util');
-const exec = promisify(require('child_process').exec);
+const core = require("@actions/core");
+const { Octokit } = require("@octokit/rest");
+const { join: pathJoin } = require("path");
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
 
 const octokit = new Octokit({
-  auth: core.getInput('token')
+  auth: core.getInput("token"),
 });
 
 async function main() {
-
   core.info(`Repository ${process.env.GITHUB_REPOSITORY}`);
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
   // 1. Get the current version
-  const {version: newVersion} = __non_webpack_require__(pathJoin(core.getInput('path'), 'package.json'));
+  const { version: newVersion } = __non_webpack_require__(
+    pathJoin(core.getInput("path"), "package.json")
+  );
   core.info(`New version: ${newVersion}`);
 
   // 2. Get the latest release version and hash
@@ -24,16 +25,20 @@ async function main() {
   let beginningSha;
   let firstRelease = false;
   let previousVersion;
-  const {data: [latestRelease]} = await octokit.repos.listReleases({
+  const {
+    data: [latestRelease],
+  } = await octokit.repos.listReleases({
     owner,
     repo,
-    per_page: 1
+    per_page: 1,
   });
 
   // Handle scenario where there is no release yet
   if (!latestRelease) {
     firstRelease = true;
-    core.info('Existing release. Release notes will contain all commits from the beginning of the repo.');
+    core.info(
+      "Existing release. Release notes will contain all commits from the beginning of the repo."
+    );
   }
 
   // There are other releases
@@ -49,10 +54,10 @@ async function main() {
     // would be master and calling `git log` with the range `master..HEAD` would give us nothing.
     // To get a sha, we get the commit that the tag points to.
     try {
-      const {data} = await octokit.repos.getCommit({
+      const { data } = await octokit.repos.getCommit({
         owner,
         repo,
-        ref: previousVersion
+        ref: previousVersion,
       });
       beginningSha = data.sha;
       core.info(`Previous release sha: ${beginningSha}`);
@@ -65,51 +70,56 @@ async function main() {
 
   // 3. If versions are different or if its the first release
   if (firstRelease || previousVersion !== newVersion) {
-    
     // 1. Generate the change log
     let changeLog;
     try {
-      const {stdout, stderr} = await exec(`git log ${firstRelease ? '' : `${beginningSha}..HEAD`} --pretty=format:"- %h %s"`);
+      const { stdout, stderr } = await exec(
+        `git log ${
+          firstRelease ? "" : `${beginningSha}..HEAD`
+        } --pretty=format:"- %h %s"`
+      );
       changeLog = stdout.trim();
       core.info(`Change Log:\n${changeLog}`);
     } catch (error) {
-      core.setFailed('Unable to generate the change log.');
+      core.setFailed("Unable to generate the change log.");
       core.error(error);
       process.exit();
     }
 
     // 2. Publish a release
     try {
-      const {data} = await octokit.repos.createRelease({
-        owner, 
-        repo, 
-        tag_name: newVersion, 
-        name: newVersion, 
+      const { data } = await octokit.repos.createRelease({
+        owner,
+        repo,
+        tag_name: newVersion,
+        name: newVersion,
         target_commitish: process.env.GITHUB_SHA,
-        body: changeLog
+        body: changeLog,
       });
       // What output should we provide?
       // https://docs.github.com/en/rest/reference/repos#get-a-release
       core.info(`Created release ${newVersion}`);
-      core.setOutput('released', true);
-      core.setOutput('html_url', data.html_url);
-      core.setOutput('upload_url', data.upload_url);
-      core.setOutput('release_id', data.id);
-      core.setOutput('release_tag', data.tag_name);
-      core.setOutput('release_name', data.name);
+      core.setOutput("released", true);
+      core.setOutput("html_url", data.html_url);
+      core.setOutput("upload_url", data.upload_url);
+      core.setOutput("release_id", data.id);
+      core.setOutput("release_tag", data.tag_name);
+      core.setOutput("release_name", data.name);
     } catch (error) {
-      core.setFailed(`Failed to create release ${newVersion} for ${owner}/${repo}#${process.env.GITHUB_SHA}`);
+      core.setFailed(
+        `Failed to create release ${newVersion} for ${owner}/${repo}#${process.env.GITHUB_SHA}`
+      );
       core.error(error);
-      core.setOutput('released', true);
+      core.setOutput("released", true);
       core.debug(JSON.stringify(error.headers));
       core.debug(JSON.stringify(error.request));
       process.exit();
     }
   }
-  
+
   // Nothing to do
   else {
-    core.info('Version has not changed.');
+    core.info("Version has not changed.");
   }
 }
 
